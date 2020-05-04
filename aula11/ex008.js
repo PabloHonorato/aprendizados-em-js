a178d585bcb482480d9490627418a798bb38c734
Pablo Honorato <pblhonoratogmail.com>
2020-05-04 12:22:11 -0300

    aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 78.2
+console.log(`a velocidade do seu carro é de ${vel} km`)
+
+        //condição simples!
+if(vel > 60){ 
+    console.log('você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('dirija sempre com o cinto de segurança')
---
+++ ex009.js
@@ -0,0 +1,9 @@
+var país = 'italia'
+
+console.log(`nesceu em ${país}`)
+
+if(país == 'Brasil'){
+    console.log('voê é brasileiro!')
+}else{
+    console.log(' você é estrangeiro!')
+}
